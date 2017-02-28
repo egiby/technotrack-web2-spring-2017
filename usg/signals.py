560bdd8c778ae2072f03c9a9f49e3fa26b904b57
egiby <egiby@egiby>
2017-02-28 18:43:51 +0300

    Add garbage collector for content

--- usg/signals.py
+++ usg/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 
 from core.models import get_friends
 from .models import Post, Event
@@ -15,4 +15,12 @@ def create_feed_events(instance, created=False, *args, **kwargs):
         event.save()
 
 
+def garbage_collector(sender, instance, *args, **kwargs):
+    if Post.objects.filter(content_id=instance.content_id):
+        return
+
+    instance.content.delete()
+
+
 post_save.connect(create_feed_events, sender=Post)
+post_delete.connect(garbage_collector, sender=Post)
